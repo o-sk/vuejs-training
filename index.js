deab39c2e4f0de9523491ae4e96730954e13c9e1
o-sk <m.ohsuka@gmail.com>
2018-12-12 22:31:50 +0900

    Lesson v-bind

--- index.js
+++ index.js
@@ -3,4 +3,10 @@ var app = new Vue({
     data: {
         message: 'Hello Vue!'
     }
-})  
+})
+var app2 = new Vue({
+    el: '#app-2',
+    data: {
+        message: 'You loaded this page on ' + new Date().toLocaleString()
+    }
+})
