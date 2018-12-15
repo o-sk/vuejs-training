9fe68b0adc065c152d7e9c79fa9d35f5e6ac494a
o-sk <m.ohsuka@gmail.com>
2018-12-15 09:55:16 +0900

    Lesson computed setter

--- computed/index.js
+++ computed/index.js
@@ -9,3 +9,22 @@ var vm = new Vue({
         }
     }
 })
+var vm2 = new Vue({
+    el: '#demo',
+    data: {
+        firstName: 'Foo',
+        lastName: 'Bar'
+    },
+    computed: {
+        fullName: {
+            get: function () {
+                return this.firstName + ' ' + this.lastName
+            },
+            set: function (newValue) {
+                var names = newValue.split(' ')
+                this.firstName = names[0]
+                this.lastName = names[names.length - 1]
+            }
+        }
+    }
+})
