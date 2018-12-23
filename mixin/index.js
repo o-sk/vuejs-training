a97601d8b97235c5bdf73905e2f700f7fd2d2c3c
o-sk <m.ohsuka@gmail.com>
2018-12-23 11:45:00 +0900

    Lesson merging options

--- mixin/index.js
+++ mixin/index.js
@@ -1,16 +1,42 @@
 var myMixin = {
     created: function () {
-      this.hello()
+        this.hello()
     },
     methods: {
-      hello: function () {
-        console.log('hello from mixin!')
-      }
+        hello: function () {
+            console.log('hello from mixin!')
+        }
     }
-  }
+}
   
-  var Component = Vue.extend({
-    mixins: [myMixin]
-  })
+var Component = Vue.extend({
+  mixins: [myMixin]
+})
   
-  var component = new Component() 
+var component = new Component() 
+
+var mixin = {
+    created: function () {
+        console.log('mixin hook called')
+    },
+    data: function () {
+        return {
+            message: 'hello',
+            foo: 'abc'
+        }
+    }
+}
+  
+new Vue({
+    mixins: [mixin],
+    data: function () {
+        return {
+            message: 'goodbye',
+            bar: 'def'
+        }
+    },
+    created: function () {
+        console.log('component hook called')
+        console.log(this.$data)
+    }
+})
