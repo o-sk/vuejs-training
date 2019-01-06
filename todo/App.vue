abaef6718dc61b6664396c15d6e84e0d4024598f
o-sk <m.ohsuka@gmail.com>
2019-01-06 22:47:10 +0900

    Implement filteredTasks

--- todo/App.vue
+++ todo/App.vue
@@ -24,6 +24,17 @@
         <form @submit.prevent="addLabel">
             <input type="text" v-model="newLabelText" placeholder="new label">
         </form>
+        <h2>filter</h2>
+        <ul>
+            <li v-for="label in labels" :key="label.id">
+                <input type="radio" :checked="label.id === filter" @change="changeFilter(label.id)">
+                {{ label.text }}
+            </li>
+            <li>
+                <input type="radio" :checked="filter === null" @change="changeFilter(null)">
+                no filter
+            </li>
+        </ul>
     </div>
 </template>
 
@@ -38,10 +49,13 @@ export default {
     },
     computed: {
         tasks() {
-            return this.$store.state.tasks
+            return this.$store.getters.filteredTasks
         },
         labels() {
             return this.$store.state.labels
+        },
+        filter() {
+            return this.$store.state.filter
         }
     },
     methods: {
@@ -67,6 +81,11 @@ export default {
         getLabelText(id) {
             const label = this.labels.filter(label => label.id === id)[0]
             return label ? label.text : ''
+        },
+        changeFilter(labelId) {
+            this.$store.commit("changeFilter", {
+                filter: labelId
+            })
         }
     }
 
