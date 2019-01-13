f206bbdeb7adddcb82e45b40b328d49d910019ac
o-sk <m.ohsuka@gmail.com>
2019-01-13 11:31:57 +0900

    Add custom commands

---
+++ sample-app/test/e2e/custom-commands/enterValue.js
@@ -0,0 +1,9 @@
+exports.command = function (selector, event, keyCode) {
+  return this.execute(function (selector, event, keyCode) {
+    var e = document.createEvent('HTMLEvents')
+    if (keyCode) {
+      e.keyCode = keyCode
+    }
+    document.querySelector(selector).dispatchEvent(e)
+  }, [selector, event, keyCode])
+}
