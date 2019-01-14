3d735d986c5c709d9b142f9d4ae61bbadb96fa10
o-sk <m.ohsuka@gmail.com>
2019-01-14 19:51:30 +0900

    Implements SaLoginView

--- sample-app/src/components/templates/SaLoginView.vue
+++ sample-app/src/components/templates/SaLoginView.vue
@@ -1,3 +1,36 @@
 <template>
-  <p>Login</p>
+  <div class="login-view">
+    <h1>Sample App</h1>
+    <SaLoginForm :onlogin="handleLogin" />
+  </div>
 </template>
+
+<script>
+import SaLoginForm from '@/components/molecules/SaLoginForm.vue'
+
+export default {
+  name: 'SaLoginView',
+
+  components: {
+    SaLoginForm
+  },
+
+  methods: {
+    hadleLogin (authInfo) {
+      return this.$store.dispatch('login', authInfo)
+        .then(() => {
+          this.$router.push({ path: '/' })
+        })
+        .catch(err => this.throwReject(err))
+    },
+    throwReject (err) { return Promise.reject(err) }
+  }
+}
+</script>
+
+<style scoped>
+.login-view {
+  witdh: 320px;
+  margin: auto;
+}
+</style>
