038fa2933a5ea239d2d7064246e347e103be0315
o-sk <m.ohsuka@gmail.com>
2019-01-14 10:59:23 +0900

    Implement SaButton

---
+++ sample-app/src/components/atoms/SaButton.vue
@@ -0,0 +1,52 @@
+<template>
+  <button
+    :class="classes"
+    :disabled="disabled"
+    type="button"
+    @click="handleClick"
+  >
+    <slot />
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'SaButton',
+
+  props: {
+    type: {
+      type: String,
+      default: 'button'
+    },
+    disabled: {
+      type: Boolean,
+      default: false
+    }
+  },
+
+  computed: {
+    classes () {
+      const suffix = this.type === 'text' ? ('-' + this.type) : ''
+      return [`sa-button${suffix}`]
+    }
+  },
+
+  methods: {
+    handleClick (e) {
+      this.$emit('click', e)
+    }
+  }
+}
+</script>
+
+<style scoped>
+.sa-button {
+  padding: .6em 1.3em;
+}
+.sa-button-text {
+  border: none;
+  padding-right: 0;
+  padding-left: 0;
+}
+
+</style>
